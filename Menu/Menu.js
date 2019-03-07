d22f1d9c2b9c01c283897a5f48b5bda88c15b917
maxgunter99 <maxgunter99@gmail.com>
2019-03-06 19:39:06 -0600

    the sections now expand

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -13,5 +13,3 @@ const menuButton = document.querySelector('.menu-button');
 
 // // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener('click', toggleMenu);
-
-
